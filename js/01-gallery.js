89b43dc27bc5937f97b94b7d47f996d192199009
Vitalii Kyi <kyi.profy@gmail.com>
2023-06-11 18:28:53 +0300

    finished hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const addItems = galleryItems.map((item) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" />
+    </a>
+</li>`
+).join('');
+
+gallery.insertAdjacentHTML('afterbegin', addItems);
+
+const instance = basicLightbox.create(`<img src="" alt="">`, {
+    onShow: (instance) => {
+        window.addEventListener('keydown', handleKeyPress);
+        console.log('onShow', instance);
+    },
+    onClose: (instance) => {
+        window.removeEventListener('keydown', handleKeyPress);
+        console.log('onClose', instance);
+    },
+});
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (event.target.classList.contains('gallery__image')) {
+        const largeImageUrl = event.target.dataset.source;
+        instance.element().querySelector('img').setAttribute('src', largeImageUrl);
+        instance.show();
+    }
+});
+
+function handleKeyPress(event) {
+    if (event.code === 'Escape') {
+        instance.close();
+    }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const addItems = galleryItems.map((item) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>`
+).join('');
+
+gallery.insertAdjacentHTML('afterbegin', addItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  caoptionDelay: 250,
+  scrollZoom: true,
+});
